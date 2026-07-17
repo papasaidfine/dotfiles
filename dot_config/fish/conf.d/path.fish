92d67c0cb51089e78b1b45a2caf52c21bb945b80
papasaidfine <44102846+papasaidfine@users.noreply.github.com>
2026-07-17 02:59:54 +0000

    fish: add ~/.local/bin to PATH (fish does not read ~/.profile)

--- dot_config/fish/conf.d/path.fish
+++ dot_config/fish/conf.d/path.fish
@@ -1,3 +1,9 @@
+# User-installed binaries. bash gets this from ~/.profile, which fish never
+# reads, so it must be added here explicitly.
+if test -d ~/.local/bin
+    fish_add_path ~/.local/bin
+end
+
 # Add Cargo's bin directory to PATH when present
 if test -d ~/.cargo/bin
     fish_add_path ~/.cargo/bin
